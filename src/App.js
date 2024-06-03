ea48f9fe7bf9f8e3b757f2ddc0f38b194c686da1
raksha <rakshamalviya19@gmail.com>
2024-06-03 15:49:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Design1 from './component/Design-1/Design1';
+import Design2 from './component/Design-2/Design2';
+
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Design1 />}></Route>
+        <Route path="/Mxpertz" element={<Design1 />}></Route>
+        <Route path="/cartDetails/:_id" element={<Design2 />}></Route>
+      </Routes>
+
+    </>
+  );
+}
+
+export default App;
